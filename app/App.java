1643a237d8fd18b11b8af5f775ebda9fa054d55c
Murillo-pedroso <murillomonteirop@gmail.com>
2021-03-30 21:15:25 -0300

    Classe de monstros com 3 monstros

---
+++ app/App.java
@@ -0,0 +1,32 @@
+public class App {
+    public static void main(String[] args){
+        
+        Monstro tepig = new Monstro("Tepig","fogo",65,63,45,45);
+        Monstro snivy = new Monstro("Snivy","grama",45,45,55,63);
+        Monstro oshawott = new Monstro("Oshawott","agua",55,55,45,45);  
+              
+        System.out.println("Nome: " + tepig.nome +
+                           "\nTipo: " + tepig.tipo+
+                           "\nVida: " + tepig.vida+
+                           "\nForça: " + tepig.forca+
+                           "\nDefesa: " + tepig.defesa+
+                           "\nVelocidade: " + tepig.velocidade+"\n");
+
+        System.out.println("Nome: " + snivy.nome +
+                           "\nTipo: " + snivy.tipo+
+                           "\nVida: " + snivy.vida+
+                           "\nForça: " + snivy.forca+
+                           "\nDefesa: " + snivy.defesa+
+                           "\nVelocidade: " + snivy.velocidade+"\n");
+
+        System.out.println("Nome: " + oshawott.nome +
+                           "\nTipo: " + oshawott.tipo+
+                           "\nVida: " + oshawott.vida+
+                           "\nForça: " + oshawott.forca+
+                           "\nDefesa: " + oshawott.defesa+
+                           "\nVelocidade: " + oshawott.velocidade+"\n");
+
+        
+
+    }
+}
---
+++ app/Monstro.java
@@ -0,0 +1,21 @@
+public class Monstro{
+    String nome;
+    String tipo;
+    int vida;
+    int forca;
+    int defesa;
+    int velocidade;
+
+
+    public Monstro(String nome, String tipo, int vida, int forca,int defesa, int velocidade){
+        this.nome = nome;
+        this.tipo = tipo;
+        this.vida = vida;
+        this.forca = forca;
+        this.defesa = defesa;
+        this.velocidade = velocidade;
+
+    }
+
+
+}
