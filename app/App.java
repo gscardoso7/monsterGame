31af8fa22fc6323c9f321353c76a52c2b2c9a233
João Vitor Andrade de Araujo <joaovandrade30@gmail.com>
2021-03-30 23:31:16 -0300

    Manutenção em pequenos trechos
    Alguns trechos desorganizados e atualização de uma parte não vista no último commit.

--- app/App.java
+++ app/App.java
@@ -1,16 +1,16 @@
 public class App {
     public static void main(String[] args){
         
-        Monstro tepig = new Monstro(1,"Tepig","fogo",65,63,45,45);
-        Monstro snivy = new Monstro(2,"Snivy","grama",45,45,55,63);
-        Monstro oshawott = new Monstro(3,"Oshawott","agua",55,55,45,45); 
+        Monstro tepig = new Monstro(1,"Tepig","fogo", 65, 63, 45, 45);
+        Monstro snivy = new Monstro(2, "Snivy", "grama", 45, 45, 55, 63);
+        Monstro oshawott = new Monstro(3, "Oshawott", "agua", 55, 55, 45, 45); 
         Escolha verificaEscolhaDoJogador = new Escolha();
-        
+
+        System.out.println("\tMonster Game");
+
         tepig.Imprime();
         snivy.Imprime();
         oshawott.Imprime();
-
-       
         
         verificaEscolhaDoJogador.EscolhaMonstro();
 
--- app/Escolha.java
+++ app/Escolha.java
@@ -7,37 +7,29 @@ public class Escolha {
         int escolhaJogadorUm = 0;
         int controleEscolhaJogadorUm = 1;
         
-        do {
+        do{
 
             System.out.println(" ");
             System.out.println("Faça sua " + controleEscolhaJogadorUm + "ª escolha de monstro para a batalha\n");
+            System.out.printf("Escolha: ");
 
-            System.out.println("1: Fergie de fogo");
-            System.out.println("2: Bola de água");
-            System.out.println("3: Formiga do vento");
-            System.out.println("4: Fergie de água");
-            System.out.println("5: Bola de terra");
-            System.out.println("6: Formiga do gelo");
-            System.out.printf("\nEscolha: ");
-
-
-            do {
+            do{
                 escolhaJogadorUm = leitura.nextInt();
 
-                if (escolhaJogadorUm < 1 || escolhaJogadorUm > 6){
-
-                    System.out.println("Monstro não encontrado, escolha novamente!");
-                    System.out.println("Faça sua " + controleEscolhaJogadorUm + "ª escolha de monstro para a batalha\n");
+                if(escolhaJogadorUm < 1 || escolhaJogadorUm > 6){
+                    System.out.println("\nMonstro não encontrado, escolha novamente!\n");
+                    System.out.println("Faça sua " + controleEscolhaJogadorUm + "ª escolha de algum monstro acima\n");
+                    System.out.printf("Escolha: ");
                     escolhaJogadorUm = 0;
                 }
 
-            } while(escolhaJogadorUm < 1 || escolhaJogadorUm > 6);
+
+            }while(escolhaJogadorUm < 1 || escolhaJogadorUm > 6);
             
             controleEscolhaJogadorUm++;
 
         } while (controleEscolhaJogadorUm <= 3 );
+        
+    
     }
 }
-
-
-
