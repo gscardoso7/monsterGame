11bad8876d5e0ba3d347e0b33bb4a02469f40001
João Vitor Andrade de Araujo <joaovandrade30@gmail.com>
2021-04-09 22:40:06 -0300

    Menu criado
    Menu de interação com usuário criado

--- app/App.java
+++ app/App.java
@@ -1,29 +1,34 @@
 public class App {
     public static void main(String[] args){
         
-        Monstro Beholder = new Monstro(1,"Beholder","fogo", 65, 63, 45, 45);
+        int resposta;
+
+        Monstro Beholder = new Monstro(1, "Beholder","fogo", 65, 63, 45, 45);
         Monstro Mimico = new Monstro(2, "Mimico", "grama", 45, 45, 55, 63);
         Monstro Lich = new Monstro(3, "Lich", "agua", 55, 55, 45, 45);
         Monstro Drow = new Monstro(4, "Drow", "gelo", 65, 63, 45, 45);
         Monstro Tarrasque = new Monstro(5, "Tarrasque", "agua", 65, 63, 45, 45);
         Monstro Female = new Monstro(6, "Female", "fogo", 65, 63, 45, 44);
 
-        Escolha verificaEscolhaDoJogador = new Escolha();
+        //Escolha verificaEscolhaDoJogador = new Escolha();
 
         Menu menuInteracaoComUsuario = new Menu();
-
-        menuInteracaoComUsuario.menuInteracao();
-
+        resposta = menuInteracaoComUsuario.menuInteracao();
         
+        if(resposta == 1) {
+            Beholder.Imprime();
+            Mimico.Imprime();
+            Lich.Imprime();
+            Drow.Imprime();
+            Tarrasque.Imprime();
+            Female.Imprime();
 
-        Beholder.Imprime();
-        Mimico.Imprime();
-        Lich.Imprime();
-        Drow.Imprime();
-        Tarrasque.Imprime();
-        Female.Imprime();
-        
-        // verificaEscolhaDoJogador.EscolhaMonstro();
+            // verificaEscolhaDoJogador.EscolhaMonstro();
+        }
 
+        else if (resposta == 2) {
+            System.out.println("Sessão encerrada Monster Gamer");
+            System.out.println("Até a próxima");
+        }
     }
 }
--- app/Menu.java
+++ app/Menu.java
@@ -1,8 +1,35 @@
+import java.util.Scanner;
+
 public class Menu {
-    public void menuInteracao(){
+    public int menuInteracao(){
         System.out.println("\t>>> Monster Game <<<");
 
         System.out.println("\t1- Jogar partida arcade");
         System.out.println("\t2- Sair");
+        System.out.printf("RESPOSTA: ");
+
+        Scanner leitura = new Scanner(System.in);
+        
+        int navegacaoMenu = 0;
+
+        navegacaoMenu = leitura.nextInt();
+
+        switch (navegacaoMenu) {
+
+            case 1:
+                System.out.println("");
+                break;
+
+            case 2: 
+                
+                break;
+
+            default:
+                System.out.println("Opcao não encontrada!");
+                break;
+
+        }
+
+        return navegacaoMenu;
     }
 }
